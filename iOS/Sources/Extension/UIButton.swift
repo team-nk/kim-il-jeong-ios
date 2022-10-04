0a8a7151a6fc97f2cce242cbd3340c4c5159447f
juyeong <juyeongno1@naver.com>
2022-10-04 17:12:48 +0900

    feat :: UIButtonExtension

---
+++ iOS/Sources/Extension/UIButton.swift
@@ -0,0 +1,12 @@
+import UIKit
+extension UIButton {
+    func setUnderline(start:Int) {
+        guard let title = title(for: .normal) else { return }
+        let attributedString = NSMutableAttributedString(string: title)
+        attributedString.addAttribute(.underlineStyle,
+                                      value: NSUnderlineStyle.single.rawValue,
+                                      range: NSRange(location: start, length: title.count-start)
+        )
+        setAttributedTitle(attributedString, for: .normal)
+    }
+}
