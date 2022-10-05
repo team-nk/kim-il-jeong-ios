964604541cf8c30cd54852dae2e48a7860d3034a
juyeong <juyeongno1@naver.com>
2022-10-05 12:11:17 +0900

    feat :: MapVC & MapReactor 추가

---
+++ iOS/Sources/Scene/Map/UI/MapVC.swift
@@ -0,0 +1,15 @@
+import UIKit
+import SnapKit
+import Then
+
+class MapVC : BaseVC<MapReactor> {
+    override func setLayout() {
+        
+    }
+    override func configureVC() {
+        
+    }
+    override func addView() {
+        
+    }
+}
