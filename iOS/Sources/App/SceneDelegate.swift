234027f95288d5df7b131cd688b9deed0dd4195f
juyeong <juyeongno1@naver.com>
2022-10-18 12:48:21 +0900

    feat :: 리펙토링

--- iOS/Sources/App/SceneDelegate.swift
+++ iOS/Sources/App/SceneDelegate.swift
@@ -13,8 +13,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     ) {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
-        let mainViewController = LoginVC(reactor: LoginReactor())
-        let naviMainViewController = BaseNC(rootViewController: mainViewController)
+        let mainViewController = MapVC(reactor: MapReactor())
+        let naviMainViewController = mainViewController
         window?.rootViewController = naviMainViewController
         window?.makeKeyAndVisible()
 
