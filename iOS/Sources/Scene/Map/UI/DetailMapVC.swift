3bea2edb926c4c4178361962d2c3e6aa941d2b7c
juyeong <juyeongno1@naver.com>
2022-10-18 22:04:51 +0900

    feat :: DetailMapVC

---
+++ iOS/Sources/Scene/Map/UI/DetailMapVC.swift
@@ -0,0 +1,15 @@
+import UIKit
+import SnapKit
+import Then
+import MapKit
+import RxCocoa
+import CoreLocation
+
+class DetailMapVC: UIViewController {
+    let detailLocationTabelView = UITableView()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+    
+}
