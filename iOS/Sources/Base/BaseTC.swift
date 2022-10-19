1fa7ee0b1416534c8c73140ffcd429c314fef3e5
juyeong <juyeongno1@naver.com>
2022-10-19 22:14:24 +0900

    feat :: BaseTC

--- iOS/Sources/Base/BaseTC.swift
+++ iOS/Sources/Base/BaseTC.swift
@@ -1,9 +1,26 @@
-//
-//  BaseTC.swift
-//  Kim-Il-Jeong
-//
-//  Created by 박주영 on 2022/10/19.
-//  Copyright © 2022 com.TeamNK. All rights reserved.
-//
+import UIKit
+import SnapKit
+import Then
 
-import Foundation
+class BaseTC: UITableViewCell {
+
+    typealias KimIlJeongColor = KimIlJeongAsset.Color
+    typealias KimIlJeongImage = KimIlJeongAsset.Assets
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.addView()
+        self.setLayout()
+        self.configureVC()
+    }
+
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func addView() {}
+    func setLayout() {}
+    func configureVC() {}
+
+}
