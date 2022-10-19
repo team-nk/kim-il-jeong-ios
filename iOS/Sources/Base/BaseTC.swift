fcaed2415245b6155768b0b99ec33d9d020eb42b
juyeong <juyeongno1@naver.com>
2022-10-19 22:13:21 +0900

    feat :: 리펙토링

---
+++ iOS/Sources/Base/BaseTC.swift
@@ -0,0 +1,9 @@
+//
+//  BaseTC.swift
+//  Kim-Il-Jeong
+//
+//  Created by 박주영 on 2022/10/19.
+//  Copyright © 2022 com.TeamNK. All rights reserved.
+//
+
+import Foundation
---
+++ iOS/Sources/Scene/Map/Cell/DetailLocationTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DetailLocationTableViewCell.swift
+//  Kim-Il-Jeong
+//
+//  Created by 박주영 on 2022/10/19.
+//  Copyright © 2022 com.TeamNK. All rights reserved.
+//
+
+import UIKit
+
+class DetailLocationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
