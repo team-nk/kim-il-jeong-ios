f8cc14d4479a36d8f21d06a8307f38dbeb689a73
InhyeKang <kanginhae0826@gmail.com>
2022-10-20 19:57:19 +0900

    refactor ::  주석 제거

--- iOS/Sources/Base/TabBarVC.swift
+++ iOS/Sources/Base/TabBarVC.swift
@@ -19,35 +19,14 @@ class TabBarVC: UITabBarController {
     }
 
     func setUpTabBarItem() {
-//        let calendarVC = CalendarVC(reactor: CalendarVCReactor())
-//        calendarVC.tabBarItem = UITabBarItem(
-//            title: "",
-//            image: UIImage(named: "Calendar"),
-//            selectedImage: UIImage(named: "Calendar_fill")
-//        )
-//        let mapVC = MapVC(reactor: MapVCReactor())
-//        mapVC.tabBarItem = UITabBarItem(
-//            title: "",
-//            image: UIImage(named: "Map"),
-//            selectedImage: UIImage(named: "Map_fill")
-//        )
         let postListVC = PostListVC(reactor: PostListVCReactor())
         postListVC.tabBarItem = UITabBarItem(
             title: "",
             image: UIImage(named: "Megaphone"),
             selectedImage: UIImage(named: "Megaphone_fill")
         )
-//        let myPageVC = MyPageVC(reactor: MyPageVCReactor())
-//        myPageVC.tabBarItem = UITabBarItem(
-//            title: "",
-//            image: UIImage(named: "Person"),
-//            selectedImage: UIImage(named: "Person_fill")
-//        )
         viewControllers = [
-//            calendarVC,
-//            mapVC,
             postListVC
-//            myPageVC
         ]
     }
 }
