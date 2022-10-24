66357a46dc28fc317c1ba4cee81ecc429c08649a
juyeong <juyeongno1@naver.com>
2022-10-24 22:41:33 +0900

    feat :: SignUpViewModel

--- iOS/Sources/Scene/Login&SignUp/UI/SignUpViewModel.swift
+++ iOS/Sources/Scene/Login&SignUp/UI/SignUpViewModel.swift
@@ -1,9 +1,63 @@
-//
-//  SignUpViewModel.swift
-//  Kim-Il-Jeong
-//
-//  Created by 박주영 on 2022/10/23.
-//  Copyright © 2022 com.TeamNK. All rights reserved.
-//
-
 import Foundation
+
+import RxSwift
+import RxCocoa
+
+class SignUpViewModel: ViewModel {
+
+    struct Input {
+        let emailText: Driver<String>
+        let emailCheckText: Driver<String>
+        let idText: Driver<String>
+        let paswwordText: Driver<String>
+        let paswwordCheckText: Driver<String>
+        let buttonDidTap: Signal<Void>
+    }
+
+    struct Output {
+        let error: PublishRelay<String>
+        let success: PublishRelay<Bool>
+    }
+
+    private var disposeBag = DisposeBag()
+
+    func transform(_ input: Input) -> Output {
+        let error = PublishRelay<String>()
+        let success = PublishRelay<Bool>()
+        let info = Driver.combineLatest(
+            input.emailText,
+            input.emailCheckText,
+            input.idText,
+            input.paswwordText,
+            input.paswwordCheckText)
+
+        input.buttonDidTap
+            .asObservable()
+            .flatMap { info }
+            .map {
+                if $0.0.isEmpty {
+                    return "이메일 인증을 해주세요"
+                } else if $0.1.isEmpty {
+                    return "1"
+                } else if $0.2.isEmpty {
+                    return "아이디 중복 확인을 해주세요"
+                } else if $0.3.isEmpty {
+                    return "비밀번호를 입력을 해주세요"
+                } else if $0.4.isEmpty {
+                    return "비밀번호가 다릅니다"
+                } else {
+                    return ""
+                }
+            }
+            .subscribe(onNext: {
+                if $0 == "" {
+                    error.accept($0)
+                    success.accept(true)
+                } else {
+                    error.accept($0)
+                }
+            }).disposed(by: disposeBag)
+
+        return Output(error: error, success: success)
+    }
+}
