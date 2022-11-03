8165743d2486a8eb555fcde8b5f56f2bec46e354
hsj._.06 <gtw030488@gmail.com>
2022-11-03 09:27:47 +0900

    feat :: SceneDelegate MapVC -> MainVC

--- iOS/Sources/App/SceneDelegate.swift
+++ iOS/Sources/App/SceneDelegate.swift
@@ -14,7 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
 
-        let mainViewController = MapVC()
+        let mainViewController = MainVC()
         let naviMainViewController = mainViewController
         window?.rootViewController = naviMainViewController
         window?.makeKeyAndVisible()
