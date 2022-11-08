61586fa4ae7417459086cd99ea32b55ec39d537b
juyeong <juyeongno1@naver.com>
2022-11-08 19:40:38 +0900

    feat :: BaseVC 수정

--- iOS/Sources/Base/BaseVC.swift
+++ iOS/Sources/Base/BaseVC.swift
@@ -6,8 +6,7 @@
 //
 
 import UIKit
-import ReactorKit
-
+import RxSwift
 class BaseVC: UIViewController {
     let bound = UIScreen.main.bounds
     typealias KimIlJeongColor = KimIlJeongAsset.Color
@@ -17,13 +16,13 @@ class BaseVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = KimIlJeongColor.backGroundColor2.color
+        addView()
+        setLayout()
         configureVC()
         bind()
     }
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
-        addView()
-        setLayout()
     }
 
     func addView() {}
