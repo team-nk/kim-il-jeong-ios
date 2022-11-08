5d93223a48f5de6c2e906d8a74af9b56032a8506
juyeong <juyeongno1@naver.com>
2022-11-08 19:40:17 +0900

    feat :: SceneDelegate 수정

--- iOS/Sources/App/SceneDelegate.swift
+++ iOS/Sources/App/SceneDelegate.swift
@@ -14,7 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
 
-        let mainViewController = MapVC()
+        let mainViewController = TabBarVC()
         let naviMainViewController = mainViewController
         window?.rootViewController = naviMainViewController
         window?.makeKeyAndVisible()
