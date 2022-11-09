9db5fb31e37bf73683d714c36f9ecf81fc56ba0e
hsj._.06 <gtw030488@gmail.com>
2022-11-09 22:35:47 +0900

    feat :: MainReactor

---
+++ iOS/Sources/Scene/Main/MainVC/Reactor/MainReactor.swift
@@ -0,0 +1,20 @@
+import ReactorKit
+
+class MainReactor: Reactor {
+
+    private let disposeBag: DisposeBag = .init()
+    let initialState: State
+
+    enum Action {
+    }
+    enum Mutation {
+    }
+    struct State {
+    }
+
+    init() {
+        initialState = State()
+
+    }
+
+}
