d194ce0bb91355f857338347951b2b40f36d0f01
InhyeKang <kanginhae0826@gmail.com>
2022-11-21 14:37:45 +0900

    refactor :: 코드 리팩토링

--- iOS/Sources/Base/BaseVC.swift
+++ iOS/Sources/Base/BaseVC.swift
@@ -16,13 +16,13 @@ class BaseVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = KimIlJeongColor.backGroundColor2.color
+        addView()
+        setLayout()
         configureVC()
         bind()
     }
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
-        addView()
-        setLayout()
     }
 
     func addView() {}
