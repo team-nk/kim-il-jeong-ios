875533fb19328f8798dd5f2875a9b468fcda0c23
InhyeKang <kanginhae0826@gmail.com>
2022-11-23 23:47:16 +0900

    refactor :: 코드 리팩토링

--- iOS/Sources/Extension/UiTextfiled.swift
+++ iOS/Sources/Extension/UiTextfiled.swift
@@ -2,17 +2,29 @@ import UIKit
 import SnapKit
 import Then
 extension UITextField {
+    typealias KimIlJeongColor = KimIlJeongAsset.Color
     func addLeftPadding() {
         let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 16, height: self.frame.height))
         self.leftView = paddingView
         self.leftViewMode = ViewMode.always
     }
-    func addPaddingToCommentTextField() {
-        let leftPaddingView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: self.frame.height))
-        let rightPaddingView = UIView(frame: CGRect(x: 0, y: 0, width: 60, height: self.frame.height))
+    func addPaddingToTextField(leftSize: CGFloat, rightSize: CGFloat) {
+        let leftPaddingView = UIView(frame: CGRect(x: 0, y: 0, width: leftSize, height: self.frame.height))
+        let rightPaddingView = UIView(frame: CGRect(x: 0, y: 0, width: rightSize, height: self.frame.height))
         self.leftView = leftPaddingView
         self.rightView = rightPaddingView
         self.leftViewMode = ViewMode.always
         self.rightViewMode = ViewMode.always
+    }
+    func setTextField(forTextField: UITextField, placeholderText: String) {
+        forTextField.attributedPlaceholder = NSAttributedString(string: "\(placeholderText)", attributes: [
+            .foregroundColor: KimIlJeongColor.textfieldDeactivationColor.color,
+            .font: UIFont.systemFont(ofSize: 16, weight: .light)
+        ])
+        forTextField.layer.borderWidth = 1
+        forTextField.layer.borderColor = KimIlJeongColor.mainColor.color.cgColor
+        forTextField.layer.cornerRadius = 10
+        forTextField.textColor = KimIlJeongColor.strongExplanation.color
+        forTextField.font = .systemFont(ofSize: 18.48, weight: .regular)
     }
 }
