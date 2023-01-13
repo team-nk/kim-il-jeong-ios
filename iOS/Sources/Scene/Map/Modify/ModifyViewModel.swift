e4d0c8b7524d545c22c1973f92b30810441bde65
juyeong <juyeongno1@naver.com>
2023-01-13 12:46:55 +0900

    refactor :: Modify 코드 리펙토링

--- iOS/Sources/Scene/Map/Modify/ModifyViewModel.swift
+++ iOS/Sources/Scene/Map/Modify/ModifyViewModel.swift
@@ -6,23 +6,41 @@ import RxCocoa
 class ModifyViewModel: BaseVM {
 
     struct Input {
-        let titleText: Driver<String>
+        let scheduleId: Int
+        let content: Driver<String>
+        let address: String
+        let color: Driver<String>
+        let startTime: Driver<String>
+        let endTime: Driver<String>
+        let isAlways: Driver<Bool>
+        let doneButtonDidTap: Signal<Void>
     }
 
     struct Output {
-        let title: PublishRelay<String>
+        let putScheduleResult: PublishRelay<Bool>
+        let content: PublishRelay<String>
     }
 
     private var disposeBag = DisposeBag()
 
     func transform(_ input: Input) -> Output {
-        let title = PublishRelay<String>()
+        let api = Service()
+        let putScheduleResult = PublishRelay<Bool>()
+        let info = Driver.combineLatest(input.content, input.color, input.startTime, input.endTime, input.isAlways)
+        input.doneButtonDidTap.asObservable()
+            .withLatestFrom(info)
+            .flatMap { content, color, startTime, endTime, isAlways in
+                api.putSchedule(input.scheduleId, content, input.address, color, startTime, endTime, isAlways) }
+            .subscribe(onNext: { res in
+                res == .deleteOk ? putScheduleResult.accept(true) : putScheduleResult.accept(false)
+            }).disposed(by: disposeBag)
 
-        input.titleText.asObservable()
+        let content = PublishRelay<String>()
+        input.content.asObservable()
             .map { $0.count }
             .subscribe(onNext: { length in
-                title.accept(String(length))
+                content.accept(String(length))
             }).disposed(by: disposeBag)
-        return Output(title: title)
+        return Output(putScheduleResult: putScheduleResult, content: content)
     }
 }
